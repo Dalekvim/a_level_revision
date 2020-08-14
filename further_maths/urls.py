082dc0658091cdc1638f83b46ad7fbbd85273adf
Dalekvim <dalekvim@gmail.com>
2020-08-14 21:37:22 +0100

    adding more proofs

--- further_maths/urls.py
+++ further_maths/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.FurtherMathsHomePage.as_view(), name='further-maths-home'),
-  path('proofs/', views.ProofsList.as_view(), name='further-maths-proofs'),
-  path('proofs/<int:pk>/', views.ProofDetails.as_view(), name='proof-detail'),
+  path('proof/', views.ProofsList.as_view(), name='further-maths-proofs'),
+  path('proof/<int:pk>/', views.ProofDetails.as_view(), name='proof-detail'),
 ]
