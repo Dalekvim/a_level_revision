a44ee54eb4b7acaf99217b9296027c5f6805b8e1
Dalekvim <dalekvim@gmail.com>
2020-08-15 12:16:36 +0100

    for heroku

--- a_level_revision/settings.py
+++ a_level_revision/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '192.168.0.17',
+    'a-level-revision.herokuapp.com',
+]
 
 
 # Application definition
